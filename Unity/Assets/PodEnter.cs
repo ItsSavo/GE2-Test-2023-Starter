03498b6442796529cbf95c8571d744056a347f41
C20467536 Edvinas Seporaitis <C20467536@mydit.ie>
2023-04-25 12:01:21 +0100

    Movement attempted but didnt work right

--- Unity/Assets/PodEnter.cs
+++ Unity/Assets/PodEnter.cs
@@ -33,5 +33,8 @@ public class PodEnter: MonoBehaviour
             FollowCamera.SetActive(false);
             MainCamera.SetActive(true);
         }
+
+       
+       
     }
 }
---
+++ Unity/Assets/PodMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PodMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+          
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+
+        }
+    }
+}
