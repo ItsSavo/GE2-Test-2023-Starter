897421f2e6ad2c51fab16cbcebcfb29f8df5e55a
C20467536 Edvinas Seporaitis <C20467536@mydit.ie>
2023-04-25 11:40:40 +0100

    Press F to take over pod

---
+++ Unity/Assets/PodEnter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PodEnter: MonoBehaviour
+   
+{
+    // Start is called before the first frame update
+
+    public GameObject FollowCamera;
+    public GameObject MainCamera;
+    void Start()
+    {
+        
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        FollowCamera.SetActive(true);
+        MainCamera.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            FollowCamera.SetActive(true);
+            MainCamera.SetActive(false);
+        }
+
+        
+    }
+}
